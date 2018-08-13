7d4c2953083388e85438a1325350f91151bb2042
tn16jv <tn16jv@brocku.ca>
2018-08-13 02:15:51 -0400

    Ended listener for playerModalStream.php and style changes.

--- PHP/playerModalStream.php
+++ PHP/playerModalStream.php
@@ -10,9 +10,26 @@ if (isset($_POST['playSong'])) {
     echo "<div id='playerArea'>";
     echo "<p>".rawurldecode($file)."</p>";
     $url = "song.php?user=" . $_POST['userName'] . "&fileName=" . rawurlencode($file);
-    echo "<iframe src='$url' height='150'></iframe>";
+    echo "<iframe id='theFrame' src='$url' height='150'></iframe>";
     echo "</div>";
 }
 ?>
+<script>
+    $("#theFrame").on("load", function() {
+        var contents = $("#theFrame").contents();
+        $(contents).find("audio").length ? iframeAlter("audio", contents) : iframeAlter("video", contents);
+    });
+
+    function iframeAlter(source, contents) {
+        $(contents).find(source).on("ended", function(event) { // Add 'ended' listener to the audio source in the iframe
+            next();
+        });
+
+        $(contents).find(source).css("width", "99%");
+
+        var height = $(contents).find(source).height();    // Want the frame only as tall as the source
+        $("#theFrame").css("height", String(height));
+    }
+</script>
 </body>
 </html>
